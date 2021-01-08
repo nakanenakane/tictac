34f09c410ce8eef510321a34b3e7b73ca93ca685
nakanenakane <nandakakanashe@gmail.com>
2021-01-08 23:46:29 +0900

    おためしゲーム

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations'
+   }
+   devise_scope :user do
+    get  'users/sign_up', to: 'users/registrations#new'
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
+
+  resources :top do
+    member do
+      get 'menu', to: 'top#menu'
+    end
+  end
+
+  resources :games do
+    member do
+      get 'easy', to: 'games#easy'
+      get 'normal', to: 'games#normal'
+      get 'hard', to: 'games#hard'
+    end
+  end
+  
+  resources :trial , only: [:index]
+
+  root 'top#index'
+
+
 end
