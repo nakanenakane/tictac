fcd38b53767dff783bdac4a9cb5154af9dc8e3e8
nakanenakane <nandakakanashe@gmail.com>
2021-01-11 22:13:55 +0900

    ログイン後のリンク先編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
 
- before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    menu_index_path
+  end
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:nickname, :password, :password_confirmation, :age) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:nickname, :password, :password_confirmation, :age ) }
     devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:nickname, :password, :password_confirmation) }
   end
   
