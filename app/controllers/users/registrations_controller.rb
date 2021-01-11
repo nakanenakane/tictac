4d6848ba51e161fc89b13bcd0bd2c9592e971617
nakanenakane <nandakakanashe@gmail.com>
2021-01-11 22:08:58 +0900

    ユーザー登録

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,8 +2,21 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
 
+  
+
   def new
     @user = User.new
+  end
+
+  def create
+    @user = User.new(sign_up_params)
+    if @user.save
+      session[:user_id] = @user.id
+      session["devise.regist_data"] = {user: @user.attributes}
+    redirect_to menu_path
+    else
+      render "new"
+    end
   end
 
 end
