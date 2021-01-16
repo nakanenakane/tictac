3511c685f308a98eb3ef8887c7e445dbe502f1a0
nakanenakane <nandakakanashe@gmail.com>
2021-01-16 23:34:54 +0900

    react plactice

--- tictac/src/App.js
+++ tictac/src/App.js
@@ -1,37 +1,18 @@
 import logo from './logo.svg';
+import React, { Component } from 'react';
+import Person from './person/person';
 import './App.css';
 
-function App() {
-  let name = 'Tanaka';
-  return (
-    <div>
-    <h1 className='title'>Hello</h1>
-    <p className='sub'> This is ...</p>
-    <h2>Hello {name}!</h2>
-    <div className="App">
-      <header className="App-header">
-        <img src={'/picture/kumo.png'} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        
-        
-        
-        <a
-          className="App-link"
-          href="https://www.supremenewyork.com/shop"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h1 className="sup">
-            Go To Fuck'n Supreme
-            </h1>
-        </a>
-      </header>
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Does it work?</h1>
+        <Person name='fuck'/>
+        <Person name='ORANGE'/>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ tictac/src/person/person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = (props) => {
+    return <p>This is {props.name} Component.</p>
+};
+
+export default person;
