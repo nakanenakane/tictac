b7d147b92d7f15f634a64fbe4f3aecc874b18077
nakanenakane <nandakakanashe@gmail.com>
2021-01-20 01:06:02 +0900

    DBデータ作成のためseed編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Question.create!(
+ [
+  {
+   questions: File.open('./app/assets/images/trial/trial_1.png'),
+   point: '0',
+   level_id: '4' }
+ ]
+)
+
+
+[
+  ['さんじ','correct','1'],
+  ['にじ','incorrect','1'],
+  ['じゅうじ','incorrect','1']
+].each do |content,is_answer,question_id|
+  Choise.create!(
+    { content:content, is_answer:is_answer, question_id:question_id}
+  )
+end
